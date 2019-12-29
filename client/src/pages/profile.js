ff7799a58560283269578bcce860270ea80cd209
Peitra Erdi <peitrae@gmail.com>
2019-12-29 12:14:59 +0700

    Customizing the fetch policy

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -19,5 +19,23 @@ const GET_MY_TRIPS = gql`
 `
 
 export default function Profile() {
-  return <div />;
+  const { data, loading, error } = useQuery(
+    GET_MY_TRIPS, { fetchPolicy: "network_only"}
+  );
+
+  if (loading) return <Loading />
+  if (error) return <p>ERROR: {error.message}</p>
+
+  return (
+    <Fragment>
+      <Header>My Trips</Header>
+      {data.me && data.me.trips.length ? (
+        data.me.trips.map(launch => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))
+      ) : (
+        <p>You haven't booked any trips</p>
+      )}
+    </Fragment>
+  );
 }
